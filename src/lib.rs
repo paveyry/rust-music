2da9dc25696ba2c7c7ae7e25c347589210e06afd
Pierre-Alexandre VEYRY <pa.veyry@gmail.com>
2022-12-02 00:44:47 +0000

    src: note: cleanup code and add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 pub mod note;
 
 use thiserror::Error;
-use note::NoteError;
-
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("invalid note: {0}")]
-    Note(NoteError),
+    Note(note::Invalid),
 }
--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,20 +1,29 @@
 use super::Error;
 
 #[derive(Error, Debug)]
-pub enum NoteError {
+pub enum Invalid {
     #[error("invalid pitch: {0}")]
     InvalidPitch(u8),
     #[error("invalid dynamic: {0}")]
     InvalidDynamic(u8),
+    #[error("invalid rhythm: {0}")]
+    InvalidRhythm(f64),
 }
 
+/// Represents a music note, with a pitch, a rhythm, and a dynamic (volume)
 pub struct Note {
+    /// the pitch must be between 0 and 127 (included)
     pitch: u8,
+    /// the rhythm value is a floating point value of a beat (no maximum).
+    /// Some defaults are available in the rhythms_constants submodule.
     rhythm: f64,
-    dynamic: u8, // dynamic describes the volume of a note.  
+    /// the dynamic describes the volume of a note. Some defaults are available
+    /// in the dynamics_constants submodule
+    dynamic: u8, // .
 }
 
-pub enum NoteName {
+/// Represents a note by name without a specific octave or accidental
+pub enum PitchName {
     A,
     B,
     C,
@@ -24,20 +33,22 @@ pub enum NoteName {
     G,
 }
 
-impl From<NoteName> for u8 {
-    fn from(n: NoteName) -> Self {
+impl From<PitchName> for u8 {
+    /// Converts a `PitchName` into the matching natural pitch `u8` at octave 0
+    fn from(n: PitchName) -> Self {
         match n {
-            NoteName::C => 0,
-            NoteName::D => 2,
-            NoteName::E => 4,
-            NoteName::F => 5,
-            NoteName::G => 7,
-            NoteName::A => 9,
-            NoteName::B => 11,
+            PitchName::C => 0,
+            PitchName::D => 2,
+            PitchName::E => 4,
+            PitchName::F => 5,
+            PitchName::G => 7,
+            PitchName::A => 9,
+            PitchName::B => 11,
         }
     }
 }
 
+/// Represents a note accidental
 pub enum Accidental {
     Flat,
     Natural,
@@ -45,41 +56,80 @@ pub enum Accidental {
 }
 
 impl Note {
+    /// Returns a Note with the given rhythm, pitch, and dynamic
+    /// 
+    /// # Arguments
+    /// 
+    /// * `pitch` - The pitch of the note
+    /// * `rhythm` - The rhythm value of the note
+    /// * `dynamic` - The dynamic (volume) of the note
+    /// 
+    /// # Errors
+    /// 
+    /// * `Error::Note(Invalid::Pitch)` if pitch is above `127`
+    /// * `Error::Note(Invalid::Rhythm)` if rhythm is below `0.000_001`
+    /// * `Error::Note(Invalid::Dynamic)` if dynamic is above `127`
     pub fn new(pitch: u8, rhythm: f64, dynamic: u8) -> Result<Note, Error> {
+        if rhythm < 0.000_001 {
+            return Err(Error::Note(Invalid::InvalidRhythm(rhythm)))
+        }
         match (pitch, dynamic) {
             (0..=127, 0..=127) => Ok(Note {
                 pitch,
                 rhythm,
                 dynamic,
             }),
-            (.., 0..=127) => Err(Error::Note(NoteError::InvalidPitch(pitch))),
-            _ => Err(Error::Note(NoteError::InvalidDynamic(dynamic))),
+            (.., 0..=127) => Err(Error::Note(Invalid::InvalidPitch(pitch))),
+            _ => Err(Error::Note(Invalid::InvalidDynamic(dynamic))),
         }
     }
 
-    pub fn compute_pitch(note_name: NoteName, accidental: Accidental, octave: u8) -> Result<u8, Error> {
-        let base_pitch: u8 = note_name.into();
+    /// Returns a pitch value based on the given pitch name, octave, and accidental
+    /// 
+    /// # Arguments
+    /// 
+    /// * `pitch_name` - The note name (between `A` and `F`)
+    /// * `accidental` - The accidental of the note
+    /// * `octave` - Which octave the note is in (`12` pitches per octave, 
+    ///   pitch `0` is a `C`, final pitch must be `127` max)
+    /// 
+    /// # Errors
+    /// 
+    /// Will return `Error::Note(Invalid::Pitch)` if final pitch is above `127`
+    /// or underflowed below `0` (`255`)
+    pub fn compute_pitch(
+        pitch_name: PitchName,
+        accidental: Accidental,
+        octave: u8,
+    ) -> Result<u8, Error> {
+        let base_pitch: u8 = pitch_name.into();
         let pitch = 12 * octave + base_pitch;
         if pitch > 127 {
-            return Err(Error::Note(NoteError::InvalidPitch(pitch)));
+            return Err(Error::Note(Invalid::InvalidPitch(pitch)));
         }
         match (pitch, accidental) {
             (p, Accidental::Natural) => Ok(p),
-            (127, Accidental::Sharp) => Err(Error::Note(NoteError::InvalidPitch(128))),
-            (p, Accidental::Sharp) => Ok(p+1),
-            (0, Accidental::Flat) => Err(Error::Note(NoteError::InvalidPitch(255))),
-            (p, Accidental::Flat) => Ok(p-1),
+            (127, Accidental::Sharp) => Err(Error::Note(Invalid::InvalidPitch(128))),
+            (p, Accidental::Sharp) => Ok(p + 1),
+            (0, Accidental::Flat) => Err(Error::Note(Invalid::InvalidPitch(255))),
+            (p, Accidental::Flat) => Ok(p - 1),
         }
     }
 
+    /// Returns the pitch of the note
+    #[must_use]
     pub fn pitch(self) -> u8 {
         self.pitch
     }
-    
+
+    /// Returns the rhythm value of the note
+    #[must_use]
     pub fn rhythm(self) -> f64 {
         self.rhythm
     }
 
+    /// Returns the dynamic value of the note
+    #[must_use]
     pub fn dynamic(self) -> u8 {
         self.dynamic
     }
@@ -108,19 +158,52 @@ pub mod dynamic_constants {
 
 #[cfg(test)]
 mod tests {
-    use super::{NoteName, Accidental, Note};
+    use super::{Accidental, Note, PitchName};
     #[test]
     fn pitches() {
-        assert_eq!(Note::compute_pitch(NoteName::C, Accidental::Sharp, 2).unwrap(), 25);
-        assert_eq!(Note::compute_pitch(NoteName::B, Accidental::Sharp, 1).unwrap(), 24);
-        assert_eq!(Note::compute_pitch(NoteName::C, Accidental::Flat, 2).unwrap(), 23);
-        assert_eq!(Note::compute_pitch(NoteName::B, Accidental::Natural, 1).unwrap(), 23);
-        assert_eq!(Note::compute_pitch(NoteName::C, Accidental::Natural, 2).unwrap(), 24);
-        assert_eq!(Note::compute_pitch(NoteName::D, Accidental::Natural, 2).unwrap(), 26);
-        assert_eq!(Note::compute_pitch(NoteName::E, Accidental::Natural, 2).unwrap(), 28);
-        assert_eq!(Note::compute_pitch(NoteName::E, Accidental::Sharp, 2).unwrap(), 29);
-        assert_eq!(Note::compute_pitch(NoteName::F, Accidental::Natural, 2).unwrap(), 29);
-        assert_eq!(Note::compute_pitch(NoteName::G, Accidental::Sharp, 5).unwrap(), 68);
-        assert_eq!(Note::compute_pitch(NoteName::A, Accidental::Flat, 9).unwrap(), 116);
+        assert_eq!(
+            Note::compute_pitch(PitchName::C, Accidental::Sharp, 2).unwrap(),
+            25
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::B, Accidental::Sharp, 1).unwrap(),
+            24
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::C, Accidental::Flat, 2).unwrap(),
+            23
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::B, Accidental::Natural, 1).unwrap(),
+            23
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::C, Accidental::Natural, 2).unwrap(),
+            24
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::D, Accidental::Natural, 2).unwrap(),
+            26
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::E, Accidental::Natural, 2).unwrap(),
+            28
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::E, Accidental::Sharp, 2).unwrap(),
+            29
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::F, Accidental::Natural, 2).unwrap(),
+            29
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::G, Accidental::Sharp, 5).unwrap(),
+            68
+        );
+        assert_eq!(
+            Note::compute_pitch(PitchName::A, Accidental::Flat, 9).unwrap(),
+            116
+        );
     }
 }
