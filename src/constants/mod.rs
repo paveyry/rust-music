da42f1bcddcc8eb0f491ab7382fd4301d0e3f302
Pierre-Alexandre VEYRY <pa.veyry@gmail.com>
2022-12-04 02:22:24 +0000

    src: constants: add ternary rhythms

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,13 +1,29 @@
 pub mod rhythm {
-    pub static DEMI_SEMIQUAVER: f64 = 0.25;
+    // Binary rhythms
+    pub static DEMI_SEMIQUAVER: f64 = 0.125;
+    pub static DOTTED_DEMI_SEMIQUAVER: f64 = 0.1825;
     pub static SEMIQUAVER: f64 = 0.25;
+    pub static DOTTED_SEMIQUAVER: f64 = 0.375;
     pub static QUAVER: f64 = 0.5;
+    pub static DOTTED_QUAVER: f64 = 0.75;
     pub static CROTCHET: f64 = 1.;
+    pub static DOTTED_CROTCHET: f64 = 1.5;
     pub static MINIM: f64 = 2.;
     pub static DOTTED_MINIM: f64 = 3.;
     pub static SEMIBREVE: f64 = 4.;
     pub static BREVE: f64 = 8.;
-    // TODO: Add more shortcuts, especially triplet variants
+
+    // Ternary rhythms
+    pub static TER_SEMIQUAVER: f64 = 1./6.; // 0.166
+    pub static TER_DOTTED_SEMIQUAVER: f64 = 0.25;
+    pub static TER_QUAVER: f64 = 1./3.; // 0.33
+    pub static TER_DOTTED_QUAVER: f64 = 0.5;
+    pub static TER_CROTCHET: f64 = 2./3.; // 0.66
+    pub static TER_DOTTED_CROTCHET: f64 = 1.;
+    pub static TER_MINIM: f64 = 4./3.; // 1.33
+    pub static TER_DOTTED_MINIM: f64 = 2.;
+    pub static TER_SEMIBREVE: f64 = 8./3.; // 2.66
+    pub static TER_DOTTED_SEMIBREVE: f64 = 4.;
 }
 
 pub mod dynamic {
