d4f70bca81dbc90ab3a04fbacbc676dc35480f57
Pierre-Alexandre VEYRY <pa.veyry@gmail.com>
2023-09-05 02:36:09 +0100

    repo: readme: improve example with a canon-like song

--- examples/readme_example.rs
+++ examples/readme_example.rs
@@ -6,7 +6,8 @@ use rust_music::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Create a musical phrase that plays C-E-G (arpeggiated C Major chord) with crotchets, at MezzoForte volume
+    // Create a musical phrase that plays C-E-G (arpeggiated C Major chord)
+    // with crotchets, at MezzoForte volume
     let mut phrase_to_repeat = Phrase::new();
     phrase_to_repeat.add_note(Note::new(
         compute_pitch(NoteName::C, Accidental::Natural, 4)?,
@@ -28,16 +29,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut piano_part = Part::new(Instrument::AcousticGrandPiano);
     piano_part.add_phrase(phrase_to_repeat.clone(), 0.);
 
-    // Create a guitar part that plays the phrase from beat 2 (at the same time as piano plays the E)
-    let mut guitar_part = Part::new(Instrument::NylonGuitar);
-    guitar_part.add_phrase(phrase_to_repeat, 1.);
+    // Create a Strings part that plays the phrase from beat 0.5
+    // (at the same time as the piano but shifted half a beat)
+    let mut violins_part = Part::new(Instrument::StringEnsemble1);
+    violins_part.add_phrase(phrase_to_repeat, 0.5);
 
     // Create a score with a tempo of 60 (one beat per second) and add both parts
     let mut score = Score::new("my score", Tempo::new(60)?, None);
     score.add_part(piano_part);
-    score.add_part(guitar_part);
+    score.add_part(violins_part);
 
     // Write the score to a MIDI file for playback
-    score.write_midi_file(File::create("my_score.mid")?)?;
+    score.write_midi_file(File::create("readme_example.mid")?)?;
     Ok(())
 }
